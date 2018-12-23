ec3f422a5edeef6cef1005cce0a9ffe6b7faa2d5
tangyuandianxia <1210979340@qq.com>
2018-12-23 19:41:23 +0800

    新增

---
+++ Hello ThreeJS/bower_components/dist/js/url-utils.js
@@ -0,0 +1,68 @@
+/**
+ * 解析URL参数
+ * @param item
+ * @returns {Array|{index: number, input: string}}
+ */
+function get_url_parameter(item) {
+    var sValue = decodeURI(location.search).match(new RegExp("[\?\&]" + item + "=([^\&]*)(\&?)", "i"));
+    return sValue ? sValue[1] : sValue;
+}
+
+/**
+ * 获取url端口号
+ * @returns {string}
+ */
+function get_url_prot() {
+    return window.location.port;
+}
+
+/**
+ * 问号后面的部分
+ */
+function get_url_search() {
+   return window.location.search;
+}
+
+function formatDate(nows) {
+    var now = new Date(nows);
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1;
+    var date = now.getDate();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    return year + "-" + month + "-" + date + " " + hour + ":" + minute + ":" + second;
+}
+Date.prototype.pattern=function(fmt) {
+    var o = {
+        "M+" : this.getMonth()+1, //月份
+        "d+" : this.getDate(), //日
+        "h+" : this.getHours()%12 == 0 ? 12 : this.getHours()%12, //小时
+        "H+" : this.getHours(), //小时
+        "m+" : this.getMinutes(), //分
+        "s+" : this.getSeconds(), //秒
+        "q+" : Math.floor((this.getMonth()+3)/3), //季度
+        "S" : this.getMilliseconds() //毫秒
+    };
+    var week = {
+        "0" : "/u65e5",
+        "1" : "/u4e00",
+        "2" : "/u4e8c",
+        "3" : "/u4e09",
+        "4" : "/u56db",
+        "5" : "/u4e94",
+        "6" : "/u516d"
+    };
+    if(/(y+)/.test(fmt)){
+        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    }
+    if(/(E+)/.test(fmt)){
+        fmt=fmt.replace(RegExp.$1, ((RegExp.$1.length>1) ? (RegExp.$1.length>2 ? "/u661f/u671f" : "/u5468") : "")+week[this.getDay()+""]);
+    }
+    for(var k in o){
+        if(new RegExp("("+ k +")").test(fmt)){
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+        }
+    }
+    return fmt;
+}
